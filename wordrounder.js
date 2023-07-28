7ec13e145e7c0e76db08ddee242c7c6524d23764
BrunoSnts <117349129+BrunoSnts@users.noreply.github.com>
2023-07-28 12:41:50 +1000

    Update wordrounder.js

--- wordrounder.js
+++ wordrounder.js
@@ -2,7 +2,7 @@ var readline = require('readline');
 var fs = require('fs');
 
 var myInterface = readline.createInterface({
-  input: fs.createReadStream('output.js')
+  input: fs.createReadStream('output.txt')
 });
 
 myInterface.on('line', function (line) {
